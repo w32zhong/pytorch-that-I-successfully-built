581f67cd92c1fd4f7ccb73508c9ce720e1fd5574
w32zhong <clock126@126.com>
2023-11-25 17:16:59 -0500

    added expand_headers_struct in extract_build_structure.py

--- extract_build_structure.py
+++ extract_build_structure.py
@@ -1,14 +1,16 @@
 prefix = '/home/tk/Desktop/nvme0n1/pytorch-that-I-successfully-built/'
 
 
-def extract(tokens, option):
-    if option in tokens:
-        i = tokens.index(option)
-        tok = tokens[i+1]
-        tok = tok.replace(prefix, '')
-        return [tok]
-    else:
-        return []
+def extract(tokens, option, preserve=False):
+    results = []
+    for i, tok in enumerate(tokens):
+        if tok == option:
+            tok = tokens[i+1]
+            tok = tok.replace(prefix, '')
+            if preserve:
+                results.append(option)
+            results.append(tok)
+    return results
 
 
 def extract_startswith(tokens, prefix):
@@ -37,24 +39,65 @@ def common_prefix(m):
     return [s1 + '*']
 
 
-with open('build-stage1.log') as fh:
-    for line in fh:
-        line = line.strip()
-        tokens = line.split()
+def print_simple_lines():
+    with open('build-stage1.log') as fh:
+        for line in fh:
+            line = line.strip()
+            tokens = line.split()
 
-        src = extract(tokens, '-c')
-        dst = extract(tokens, '-o')
+            src = extract(tokens, '-c')
+            dst = extract(tokens, '-o')
 
-        slibs = extract_startswith(tokens, '-l')
-        dlibs = extract_endswith(tokens, '.so')
-        libs = list(set(slibs + dlibs) - set(dst))
+            slibs = extract_startswith(tokens, '-l')
+            dlibs = extract_endswith(tokens, '.so')
+            libs = list(set(slibs + dlibs) - set(dst))
 
-        objs = extract_endswith(tokens, '.o')
-        objs_dir = common_prefix(objs)
+            objs = extract_endswith(tokens, '.o')
+            objs_dir = common_prefix(objs)
 
-        if not dst or dst[0].endswith('.o') or 'bin/' in dst[0]:
-            pass
-        elif dst[0] in objs_dir:
-            print(src + libs, '==>', dst)
-        else:
-            print(src + objs_dir + libs, '==>', dst)
+            if not dst or dst[0].endswith('.o') or 'bin/' in dst[0]:
+                pass
+            elif dst[0] in objs_dir:
+                print(src + libs, '==>', dst)
+            else:
+                print(src + objs_dir + libs, '==>', dst)
+
+
+def expand_headers(src_file='tensor_new.cpp'):
+    import subprocess
+    with open('build-stage1.log') as fh:
+        for line in fh:
+            line = line.strip()
+            tokens = line.split()
+            src = extract(tokens, '-c')
+            if src and src[0].endswith(src_file):
+                isystem = extract(tokens, '-isystem', True)
+                includes = extract_startswith(tokens, '-I')
+                cc = tokens[1]
+                src = src[0]
+                cmd = [cc, '-H', '-c', src] + includes + isystem
+                output = subprocess.check_output(' '.join(cmd), shell=True, stderr=subprocess.STDOUT)
+                return output.decode().strip()
+
+
+def expand_headers_struct(src_file='tensor_new.cpp', dst_file=None):
+    output = expand_headers(src_file)
+    path = []
+    for line in output.split('\n'):
+        if len(line.split()) > 2:
+            break
+        level, header = line.split()
+        level, header = len(level) - 1, header.replace(prefix, '')
+        while level < len(path):
+            path.pop()
+        path.append(header)
+        print(path)
+
+
+if __name__ == '__main__':
+    import fire
+    fire.Fire({
+        'print_simple_lines': print_simple_lines,
+        'expand_headers': expand_headers,
+        'expand_headers_struct': expand_headers_struct
+    })
